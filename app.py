40d5d9ad1ccd8f65dc4b88620fd487ac3b88e9aa
WebsiteAuthor <126562520+edncodeismad@users.noreply.github.com>
2024-12-09 15:56:12 +0000

    Update app.py

--- app.py
+++ app.py
@@ -13,7 +13,7 @@ nltk.download('punkt_tab')
 sparse_encoder = BM25Encoder()
 
 """
-!! use the previous response (?)
+can break down screening of responses in a separate step
 """
 
 pc = Pinecone(api_key="e6890c15-db55-4a6f-95e1-0810d25641bf")
@@ -41,7 +41,7 @@ if HYBRID:
 else:
     index = pc.Index('food-products')
 
-chat_history = []
+chat_history = ['BOT: Hello, how can I help you today?']
 
 # train the sparse encoder
 def fit_sparse_encoder(data):
@@ -61,7 +61,7 @@ def send_query(input):
     results = index.query(
         namespace='product-info',
         vector=input,
-        top_k=4,
+        top_k=3,
         include_values=False
     )
     chunk_ids = [entry['id'] for entry in results.matches]
@@ -75,7 +75,7 @@ def send_hybrid_query(input):
     results = index.query(
         vector=dense_input,
         sparse_vector=sparse_input,
-        top_k=4,
+        top_k=3,
         include_values=False
     )
     chunk_ids = [entry['id'] for entry in results.matches]
@@ -95,8 +95,6 @@ def get_cosine_score(id, input):
     score = cosine_similarity(p.reshape(1, -1), input.reshape(1, -1))
     return score[0][0]
 
-prev_response = 'Hi, how can I help you today?'
-
 #@app.route('/')
 #def serve_frontend():
 #    return send_from_directory(app.static_folder, 'index.html')
@@ -114,12 +112,12 @@ def chat():
     global prev_response, chat_history
     request_data = request.json
     user_message = request_data.get('message', '')
-    chat_history.append(user_message)
-    joined_history = '\n'.join(chat_history)
+    chat_history.append(f'USER: {user_message}')
+    joined_history = '\n'.join(chat_history[-4:])
     #pre_input = f"Create a concise one-line prompt to feed into a vector similarity search to get a relevant product for this query. Strip all unnecessary conversations and words such as 'I am looking for...', 'Search for' etc. and only keep useful keywords. If reference is made to multiple different products, only create a query based on the last product mentioned in the queries:\nSequence of queries: {joined_history}\n{user_message}"
-    system_prompt = "You are tasked with creating a concise, one-line query for a vector similarity search to find the most relevant product for a user's request. Follow these rules: \n(1) Strip unnecessary words like 'I am looking for...' or 'Search for,' keeping only essential keywords. DO NOT USE WORDS SUCH AS 'SEARCH FOR'. \n(2) If reference is made to multiple different products, only create a query based on the last product mentioned in the queries, for example the sequence ['chicken', 'pasta'] would result in 'pasta' since it is the last product mentioned. The sequence ['I am loking for a cutting board', 'something cheaper?'] would result in a query like 'cheap cutting board' since this combines the relevant parts of the specific product that the user is looking for. \n(4) If the last query is not product-related or irrelevant or nonsensical (e.g., 'How are you?' or 'What's your name?' or 'abcdefsle'), respond only with 'irrelevant' and nothing else. Note that single words such as 'chicken', 'soy sauce' are relevant."
-    prompt = f"Here is the sequence of user queries, separated by newlines (most recent come last): \n{joined_history}"
-    chat_history = chat_history[-3:]
+    #system_prompt = "You are tasked with creating a concise, one-line query for a vector similarity search to find the most relevant product for a user's request. Follow these rules: \n(1) Strip unnecessary words like 'I am looking for...' or 'Search for,' keeping only essential keywords. DO NOT USE WORDS SUCH AS 'SEARCH FOR'. \n(2) If reference is made to multiple different products, combine the queries for the last product mentioned, for example the sequence ['chicken', 'pasta', 'something cheaper?'] would result in 'cheaper pasta' since it combines information about the last product mentioned.\n(4) If the last query is not product-related or irrelevant or nonsensical (e.g., 'How are you?' or 'What's your name?' or 'abcdefsle'), respond only with 'irrelevant' and nothing else. Note that single words such as 'chicken', 'soy sauce' are relevant."
+    system_prompt = "You are tasked with creating a concise, one-line query for a vector similarity search to find the most relevant product for a user's request. Follow these rules: \n(1) Strip unnecessary words like 'I am looking for...' or 'Search for,' keeping only essential keywords. DO NOT USE WORDS SUCH AS 'SEARCH FOR'. Your final answer should just be a few keywords relating to the product the user is searching for.\n(2)Combine the different parts of the conversation to identify what the last product that the user is looking for is. Consider both your previous responses and the user's inputs. Ignore previous conversations if they are not relevant to the last product being discussed. \n(3) If the last query entered by the user is not product-related or irrelevant or nonsensical (e.g., 'How are you?' or 'What's your name?' or 'abcdefsle'), respond only with 'irrelevant' and nothing else. Note that single words such as 'chicken', 'soy sauce' are relevant."
+    prompt = f"Generate the vector search prompt. Here is the conversation history, separated by newlines (most recent come last): \n{joined_history}"
     search_query = llm.write_message(prompt, system_prompt)
 
     if search_query == 'irrelevant':
@@ -141,7 +139,7 @@ def chat():
         product_results = [get_info(id) for id in ids]
         joined_results = '\n'.join(product_results)
         system_prompt = f"Answer as a helpful ecommerce store assistant in one or two short sentences. Only answer questions related to your food products. Only pick the most relevant product from the list. Don't make up answers - if you don't have the information, ask the user to rephrase their question. Remember that you are assisting the user on a website, so do not make mention of the physical store eg. 'the fruit section'. If the URL is passed to you, include the link to the product with this syntax: **[Name of product](url-link-to-product)**. If the user's message is not relevant to any of the products mentioned, say that you don't have the information and ask the user to rephrase their question, and DO NOT use any of the product information in your answer."
-        prompt = f"Respond to this user query: {user_message} \n\nRespond based on these product results, separated by newlines: \n{joined_results}"# \n\nYour previous response was: {prev_response}"
+        prompt = f"Respond to this user query: {user_message} \n\nRespond based on these product results (separated by newlines): \n{joined_results}"# \n\nYour previous response was: {prev_response}"
         response = llm.write_message(prompt, system_prompt)
 
         scores = []
@@ -158,7 +156,7 @@ def chat():
             img = f'images/{chosen_id}.png'
             link = data.loc[data[ID_COL] == chosen_id][URL_COL].values[0]
     
-    prev_response = response
+    chat_history.append(f'BOT: {response}')
 
     return jsonify({
         "response": response,
